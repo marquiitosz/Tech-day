8cc201a99827cd45476b2206fb00a7ec12a1fdf6
marcosantos1233 <marco.santos@samisaude.com>
2026-05-12 19:45:14 -0300

    chore: organize unused files

--- frontend/src/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-  return (
-    <footer className="bg-blue-600 text-white text-center p-4 mt-10">
-      <p>© 2026 EmpreendaFácil - Todos os direitos reservados</p>
-    </footer>
-  )
-}
-
-export default Footer
